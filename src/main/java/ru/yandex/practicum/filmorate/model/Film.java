5415ff0a63effb40f368de48fb5798c60b8bf291
Valentine B <vanta11a@yandex.ru>
2025-01-09 09:35:16 +0500

    Доработки по 10 спринту с учетом замечаний и смены репа

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    Long id;
+
+    @NotNull(message = "Не передано название")
+    @NotBlank(message = "Название не должно быть пустым")
+    String name;
+
+    @Size(max = 200, message = "Превышен максимальный размер описания")
+    String description;
+
+    LocalDate releaseDate;
+
+    Long duration;
 }
