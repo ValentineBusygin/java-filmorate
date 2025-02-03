625ff42def815acdf0084dfafda219c1c27e8c57
Valentine B <vanta11a@yandex.ru>
2025-02-03 11:27:44 +0500

    Перевел тесты с контроллеров на сервисы

--- src/test/java/ru/yandex/practicum/filmorate/UserServiceTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTests.java
@@ -3,46 +3,49 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.Collection;
 
-public class UserControllerTests {
+public class UserServiceTests {
 
     @Test
     void getUsers() {
-        UserController uController = new UserController();
+        UserService uService = new UserService(new InMemoryUserStorage());
         User user = new User();
         user.setEmail("user1@example.com");
         user.setLogin("user1");
         user.setBirthday(LocalDate.now().minusDays(1));
-        User createdUser = uController.create(user);
+        User createdUser = uService.create(user);
         assertEquals(createdUser.getId(), 1, "Ожидалось, что ID пользователя будет 1");
 
-        Collection<User> uCollection = uController.findAll();
+        Collection<User> uCollection = uService.findAll();
         assertEquals(uCollection.size(), 1, "Ожидалось, что в наборе будет один пользователь");
         assertEquals(createdUser, uCollection.stream().findFirst().get(), "Ожидалось, что пользователь в массиве совпадет с созданным");
     }
 
     @Test
     void updateUser() {
-        UserController uController = new UserController();
+        UserService uService = new UserService(new InMemoryUserStorage());
         User user = new User();
         user.setEmail("user1@example.com");
         user.setLogin("user1");
         user.setBirthday(LocalDate.now().minusDays(1));
-        User createdUser = uController.create(user);
+        User createdUser = uService.create(user);
         assertEquals(createdUser.getId(), 1, "Ожидалось, что ID пользователя будет 1");
 
-        Collection<User> uCollection = uController.findAll();
+        Collection<User> uCollection = uService.findAll();
         assertEquals(uCollection.size(), 1, "Ожидалось, что в наборе будет один пользователь");
         assertEquals(createdUser, uCollection.stream().findFirst().get(), "Ожидалось, что пользователь в массиве совпадет с созданным");
 
         createdUser.setEmail("user1updated@example.com");
-        User updatedUser = uController.update(createdUser);
+        User updatedUser = uService.update(createdUser);
 
-        uCollection = uController.findAll();
+        uCollection = uService.findAll();
         assertEquals("user1updated@example.com", uCollection.stream().findFirst().get().getEmail(), "Ожидалось, что email будет обновлен");
     }
 }
